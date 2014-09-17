322f0fe5cae9369fca894b962aa3f646412433b4
shadowjack <kostyap@hotmail.ru>
2014-09-17 18:48:29 +0400

    stylefixes according to rubocop

--- app/models/progress_entry.rb
+++ app/models/progress_entry.rb
@@ -1,29 +1,35 @@
+# class: model that repreents data about each game played by player
+# Consists of:
+#       user_id - player that this result belongs to
+#       nsteps - current game level
+#       result - "s13-10 p7-5 ..." - type, all occurences,
+#       right guessed occurences
 class ProgressEntry < ActiveRecord::Base
   belongs_to :user
   validates :user_id, :result, :nsteps, presence: true
-  
+
   after_save :update_user_score, on: :create
-  #result: "s13-10 p7-5 ..."
+
   protected
-    def update_user_score
-      logger.debug "After ProgressEntry has been created: " + self.inspect
-      results = self.result.split(" ").map{|res| res[1..-1]}
-      coeff = 1 + self.nsteps * (results.count - 1)
-      all = 0
-      right = 0
-      results.each do |res|
-        all += res.split("-")[0].to_i
-        right += res.split("-")[1].to_i
-      end
-      user = self.user
-      accuracy = (right * 100 / all ).floor
-      user.score += coeff * accuracy
-      logger.debug "User is up in score: " + (coeff * accuracy).to_s
-      if accuracy >= 80 && self.nsteps < 6 # поднимаем уровень сложности автоматически
-        user.options = (self.nsteps + 1).to_s + user.options[1..-1]
-      elsif accuracy <= 30 && self.nsteps > 2
-        user.options = (self.nsteps - 1).to_s + user.options[1..-1]
-      end
-      user.save
+
+  def update_user_score
+    results = result.split(' ').map { |res| res[1..-1] }
+    coeff = 1 + nsteps * (results.count - 1)
+    all = 0
+    right = 0
+    results.each do |res|
+      all += res.split('-')[0].to_i
+      right += res.split('-')[1].to_i
     end
+
+    user = self.user
+    accuracy = (right * 100 / all).floor
+    user.score += coeff * accuracy
+    if accuracy >= 80 && nsteps < 6 # automatically level up
+      user.options = (nsteps + 1).to_s + user.options[1..-1]
+    elsif accuracy <= 30 && nsteps > 2
+      user.options = (nsteps - 1).to_s + user.options[1..-1]
+    end
+    user.save
+  end
 end
