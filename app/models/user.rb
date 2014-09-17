47ce9b8ade47af2610559cf13a24596fe149d46b
shadowjack <kostyap@hotmail.ru>
2014-09-17 18:52:47 +0400

    now I'm syntactically awsome=)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
+# Model that consists of data about player
+# vk_id - id user in vk.com
+# score - current game score
+# options - current game options such as difficulty level and types
 class User < ActiveRecord::Base
   has_many :progress_entries, dependent: :destroy
   validates :vk_id, :score, :options, presence: true
