942a272223dbe1343c357a0e46836bc7bdc2247f
shadowjack <kostyap@hotmail.ru>
2014-09-17 18:59:21 +0400

    reformatted, added some comments

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,13 @@
+# Helpers for user views
 module UsersHelper
+  # Makes a button for particuar opt - option
+  # and disables it if it is not in list of available options
   def button_for_option(opt, options)
-    labels = {"s" => "S: звук", "p" => "K: позиция", "f" => "D: форма", "c" => "L: цвет"}
+    labels = { 's' => 'S: звук',
+               'p' => 'K: позиция',
+               'f' => 'D: форма',
+               'c' => 'L: цвет' }
     disable = !(options.include? opt)
-    button_tag labels[opt], value: opt, disabled: disable, class: "ctrl-btn"
+    button_tag labels[opt], value: opt, disabled: disable, class: 'ctrl-btn'
   end
 end
