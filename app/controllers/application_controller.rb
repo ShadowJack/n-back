6674b84e2a6f40d9a7768bf2cbb23589fc93e4ca
shadowjack <kostyap@hotmail.ru>
2014-09-17 19:07:55 +0400

    style tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,27 @@
+#   get '/leaders', to: 'application#show_leaders', as: :show_leaders
+#   get '/rules', to: 'application#show_rules', as: :show_rules
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  
-#   get '/play', to: 'application#play', as: :play
-#   get '/options', to: 'application#show_options', as: :show_options
-#   post '/options', to: 'application#save_options', as: :save_options
-#   get '/leaders', to: 'application#show_leaders', as: :show_leaders
-#   get '/rules', to: 'application#show_rules', as: :show_rules
-  
+
+  # Get list of top players by score
+  # and fetch info about them from vk
   def show_leaders
     @leaders = User.all.order(score: :desc).limit(30)
-    logger.debug "Leaders: " + @leaders.inspect
-    app = VK::Application.new app_id: 4485055, app_secret: 'bTODEWKsNb6ICU1CcrJZ'
-    @leaders_info =  app.users.get user_ids: @leaders.map{|l| l.vk_id.to_s}, fields: ["photo_50"], lang: "ru"
-    logger.debug "Leaders: " + @leaders_info.to_s
+    app = VK::Application.new app_id: 4_485_055, app_secret: 'bTODEWKsNb6ICU1CcrJZ'
+    @leaders_info =  app.users.get user_ids: @leaders.map { |l| l.vk_id.to_s },
+                                   fields: ['photo_50'], lang: 'ru'
     # merge @leaders and leaders_info
     @leaders_info.each do |leader|
-      leader["score"] = @leaders.select {|l| l.vk_id == leader["uid"]}[0].score
+      leader['score'] = @leaders.select { |l| l.vk_id == leader['uid'] }[0].score
     end
     respond_to do |format|
-      format.html {render 'application/show_leaders'}
-      format.json {render json: @leaders_info}
+      format.html { render 'application/show_leaders' }
+      format.json { render json: @leaders_info }
     end
   end
-  
+
   def show_rules
   end
-  
 end
