20999c0ad8f4a8618c2732d74c0978593b0a17e3
shadowjack <kostyap3@gmail.com>
2020-03-28 12:56:29 +0300

    Clean up configs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 #   get '/leaders', to: 'application#show_leaders', as: :show_leaders
 #   get '/rules', to: 'application#show_rules', as: :show_rules
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   # Get list of top players by score
   # and fetch info about them from vk
   def show_leaders
     @leaders = User.all.order(score: :desc).limit(30)
-    app = VK::Application.new app_id: 4_485_055, app_secret: 'bTODEWKsNb6ICU1CcrJZ'
+    app = VK::Application.new app_id: Rails.application.config.vk_app_id, app_secret: Rails.application.config.vk_app_secret
     @leaders_info =  app.users.get user_ids: @leaders.map { |l| l.vk_id.to_s },
                                    fields: ['photo_50'], lang: 'ru'
     # merge @leaders and leaders_info
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.vk_app_id = 4485055
+  config.vk_app_secret = 'bTODEWKsNb6ICU1CcrJZ'
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
